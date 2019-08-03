73e9cf0f36cb1aefe44b0171b1d473beff59d9c8
AlanWater <tianxingsy@gmail.com>
2019-08-03 16:16:07 +0800

    circle

--- src/animation.js
+++ src/animation.js
@@ -1,52 +1,57 @@
-function main() {
-    (function () {
-        var start = null,
-            preTime = null,
-            r = 100,
-            dom = null,
-            Ox = window.outerWidth / 2,
-            Oy = window.outerHeight / 2,
-            deg = 0;
-        function circle(timestamp) {
-            if (!start) {
-                start = timestamp;
-                preTime = timestamp;
-                if (dom) {
-                    dom.remove();
+var Animation = {
+    Circle: function (dom, Ox, Oy, r, deg) {
+        var status = 'sleep',
+            r = r || 100,
+            dom = dom || null,
+            Ox = Ox || window.outerWidth / 2,
+            Oy = Oy || window.outerHeight / 2,
+            deg = deg || 0;
+        var create = function () {
+            dom = document.createElement("div");
+            dom.style.backgroundColor = 'red';
+            dom.style.height = '10px';
+            dom.style.width = '10px';
+            dom.style.borderRadius = '50%';
+            dom.style.position = "absolute";
+            dom.style.left = Ox + 'px';
+            dom.style.top = Oy + 'px';
+            dom.style.zIndex = '9999';
+            document.body.appendChild(dom);
+        }
+        if (!dom) {
+            create();
+        }
+        this.run = function () {
+            status = 'active';
+            var start = null,
+                preTime = null;
+            function circle(timestamp) {
+                if (!start) {
+                    start = timestamp;
+                    preTime = timestamp;
                 }
-                dom = document.createElement("div");
-                dom.id = 'circle';
-                dom.style.backgroundColor = 'red';
-                dom.style.height = '10px';
-                dom.style.width = '10px';
-                dom.style.borderRadius = '50%';
-                dom.style.position = "absolute";
-                dom.style.left = Ox + 'px';
-                dom.style.top = Oy + 'px';
-                dom.style.zIndex = '9999';
-                document.body.appendChild(dom);
-                var O = document.createElement("div");
-                O.id = 'circle-o';
-                O.style.backgroundColor = 'red';
-                O.style.height = '3px';
-                O.style.width = '3px';
-                O.style.position = "absolute";
-                O.style.left = Ox + 'px';
-                O.style.top = Oy + 'px';
-                O.style.zIndex = '9999';
-                document.body.appendChild(O);
-            }
-            if (timestamp - preTime > 16.6) {
+                // if (timestamp - preTime > 100) {
                 var _x = Math.sin(deg * Math.PI / 180) * r * -1,
                     _y = Math.cos(deg * Math.PI / 180) * r * -1;
                 dom.style.left = _x + Ox + 'px';
                 dom.style.top = _y + Oy + 'px';
                 preTime = timestamp;
-                deg--;
+                deg -= 2;
+                // }
+                if (status == 'active') {
+                    requestAnimationFrame(circle);
+                }
             }
-            requestAnimationFrame(circle);
+            circle(performance.now());
         }
-        circle(performance.now());
-    })()
+        this.stop = function () {
+            status = 'sleep';
+        }
+        this.destory = function () {
+            dom && dom.remove();
+            status = 'sleep';
+        }
+    }
 }
-export default main;
+
+export default Animation;
