c61fedf8b76f990c94914fc3ef840665e9df796a
AlanWater <tianxingsy@gmail.com>
2019-08-12 16:04:12 +0800

    save

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import Substance from './substance/substance';
-
-class App extends Component {
+import './style/index.scss';
+export class AppState {
+    substanceList: Substance[];
+}
+class App extends Component<any, AppState> {
+    constructor(props: any) {
+        super(props);
+        this.state = new AppState();
+    }
+    stop = () => {
+        const { substanceList } = this.state;
+        for (let idx = 0; idx < substanceList.length; idx++) {
+            let substance = substanceList[idx];
+            substance.stop();
+        }
+    }
+    start = () => {
+        const { substanceList } = this.state;
+        for (let idx = 0; idx < substanceList.length; idx++) {
+            let substance = substanceList[idx];
+            substance.start();
+        }
+    }
+    componentDidMount() {
+        let substanceList = [];
+        for (let idx = 0; idx < 99; idx++) {
+            let substance = new Substance(document.querySelector('.panel'), 'circle');
+            substance.run();
+            substanceList.push(substance);
+        }
+        this.setState({
+            substanceList
+        })
+    }
     render() {
-        let substance = new Substance('enemy');
-        substance.run();
         return (
-            <div>app</div>
+            <div>
+                <button onClick={this.start}>启动</button>
+                <button onClick={this.stop}>暂停</button>
+                <div className="panel">
+                </div>
+            </div>
         );
     }
 }
--- src/substance/substance.ts
+++ src/substance/substance.ts
@@ -1,39 +1,45 @@
-export interface Lay{
-    x:number;
-    y:number;
+export interface Lay {
+    x: number;
+    y: number;
 }
+export type Direction = 'up' | 'leftUp' | 'rightUp' | 'left' | 'right' | 'down' | 'leftDown' | 'rightDown';
+const directions: Array<Direction> = ['up', 'leftUp', 'rightUp', 'left', 'right', 'down', 'leftDown', 'rightDown'];
 class Substance {
     private moveStatus: 'moving' | 'stop' | string = 'stop';
     private type: string = '';
     private name: string = '';
-    private ref:HTMLElement = null;
-    private id:number = 0;
-    private x:number = window.outerWidth / 2;
-    private y:number = window.outerHeight / 2;
-    private direction: 'up'|'leftUp'|'rightUp'|'left'|'right'|'down'|'leftDown'|'rightDown' = "down";
-    private speed:number = 1;
+    private ref: HTMLElement = null;
+    private id: number = 0;
+    private x: number = 0;
+    private y: number = 0;
+    private direction: Direction = "down";
+    private speed: number = 5;
+    private panel: HTMLElement;
+    private panelWidth = 0;
+    private panelHeight = 0;
     private static circleSequenceNum = 10000;
     private static circleSequenceId = 0;
-    private static refMap:{
-        [id:string]:HTMLElement;
+    private static refMap: {
+        [id: string]: HTMLElement;
     } = {};
-    private static increaseId = ()=>{
+    private static increaseId = () => {
         Substance.circleSequenceId = Substance.circleSequenceId % Substance.circleSequenceNum + 1;
         return Substance.circleSequenceId;
     }
-    private static createObjectRef = (type:string,id:number,lay:Lay)=>{
+    private static createObjectRef = (substance: Substance) => {
+        const { type, id, x, y, panel } = substance;
         //保证系统稳定，设置了最大限制，id是循环的，先移除原来的id
         const preRef = document.getElementById(`${type}${id}`);
-        if(preRef){
+        if (preRef) {
             preRef.remove();
             delete Substance.refMap[id]
         }
         //必须有一个容器，才能在容器中挂载物件，默认为body
-        const container = document.querySelector('body');
+        const container = panel;
         let ref = document.createElement('div');
-        ref.style.position='absolute';
-        ref.style.left = lay.x + 'px';
-        ref.style.top = lay.y + 'px';
+        ref.style.position = 'absolute';
+        ref.style.left = x + 'px';
+        ref.style.top = y + 'px';
         ref.id = `${type}${id}`;
         ref.className = `substance-${type}`;
         container.appendChild(ref);
@@ -41,142 +47,182 @@ class Substance {
         Substance.refMap[id] = ref;
         return ref;
     }
-    constructor(type:string,name?:string,moveStatus?:string,x?:number,y?:number,speed?:number){
+    constructor(panel: HTMLElement, type: string, name?: string, moveStatus?: string, x?: number, y?: number, speed?: number) {
         this.type = type;
+        this.panel = panel;
+        this.panelWidth = this.panel.offsetWidth;
+        this.panelHeight = this.panel.offsetHeight;
         name && (this.name = name);
         moveStatus && (this.moveStatus = moveStatus);
-        x && (this.x = x);
-        y && (this.y = y);
+        this.x = x || this.panelWidth / 2;
+        this.y = y || this.panelHeight / 2;
         speed && (this.speed = speed);
+        this.direction = directions[Math.floor(Math.random() * 8)];
         this.id = Substance.increaseId();
-        this.ref = Substance.createObjectRef(type,this.id,{x:this.x,y:this.y});
+        this.ref = Substance.createObjectRef(this);
+    }
+    inPanel = (x: number, y: number) => {
+        if ((x >= 0) && (x + 20 <= this.panelWidth) && (y >= 0) && (y + 20 <= this.panelHeight)) {
+            return true;
+        } else {
+            return false;
+        }
     }
     //运行对象
-    public run (){
-        const animation = (timestamp:number) =>{
-            const { moveStatus,id } = this;
-            switch (moveStatus){
-                case 'up':{
-                    this.y-=this.speed;
-                    break;
+    public run() {
+        var preTimestamp = null,
+            k = Math.round(Math.random() * 10) / 10 || 0.45;
+        this.moveStatus = 'moving';
+        const animation = (timestamp: number) => {
+            if (!preTimestamp) {
+                preTimestamp = timestamp;
+            }
+            const { moveStatus, id, direction } = this;
+            let x = 0, y = 0;
+            if (moveStatus == 'moving') {
+                switch (direction) {
+                    case 'up': {
+                        x = this.x;
+                        y = this.y - this.speed * k;
+                        break;
+                    }
+                    case 'down': {
+                        x = this.x;
+                        y = this.y + this.speed * k;
+                        break;
+                    }
+                    case 'leftUp': {
+                        x = this.x - this.speed * k;
+                        y = this.y - this.speed * k;
+                        break;
+                    }
+                    case 'rightUp': {
+                        x = this.x + this.speed * k;
+                        y = this.y - this.speed * k;
+                        break;
+                    }
+                    case 'left': {
+                        x = this.x - this.speed * k;
+                        y = this.y;
+                        break;
+                    }
+                    case 'right': {
+                        x = this.x + this.speed * k;
+                        y = this.y;
+                        break;
+                    }
+                    case 'leftDown': {
+                        x = this.x - this.speed * k;
+                        y = this.y + this.speed * k;
+                        break;
+                    }
+                    case 'rightDown': {
+                        x = this.x + this.speed * k;
+                        y = this.y + this.speed * k;
+                        break;
+                    }
                 }
-                case 'down':{
-                    this.y+=this.speed;
-                    break;
+                if (this.inPanel(x, y)) {
+                    this.x = x;
+                    this.y = y;
+                } else {
+                    this.direction = directions[Math.floor(Math.random() * 8)];
                 }
-                case 'leftTop':{
-                    this.x-=this.speed;
-                    this.y-=this.speed;
-                    break;
-                }
-                case 'rightTop':{
-                    this.x+=this.speed;
-                    this.y-=this.speed;
-                    break;
-                }
-                case 'left':{
-                    this.x -= this.speed;
-                    break;
-                }
-                case 'right':{
-                    this.x += this.speed;
-                    break;
-                }
-                case 'leftDown':{
-                    this.x -= this.speed;
-                    this.y += this.speed;
-                    break;
-                }
-                case 'rightDown':{
-                    this.x += this.speed;
-                    this.y += this.speed;
-                    break;
+                Substance.refMap[id].style.left = this.x + 'px';
+                Substance.refMap[id].style.top = this.y + 'px';
+                if (timestamp - preTimestamp > 100) {
+                    // this.direction = directions[Math.floor(Math.random() * 8)];
+                    preTimestamp = timestamp;
                 }
             }
-            Substance.refMap[id].style.left = this.x +'px';
-            Substance.refMap[id].style.top = this.y +'px';
             requestAnimationFrame(animation);
         }
-        animation.call(this,performance.now());
+        animation.call(this, performance.now());
+    }
+    stop = () => {
+        this.moveStatus = "stop";
+    }
+    start = () => {
+        this.moveStatus = "moving";
     }
     /**
      * Getter $moveStatus
      * @return {string }
      */
-	public get $moveStatus(): string  {
-		return this.moveStatus;
-	}
+    public get $moveStatus(): string {
+        return this.moveStatus;
+    }
 
     /**
      * Getter $type
      * @return {string }
      */
-	public get $type(): string  {
-		return this.type;
-	}
+    public get $type(): string {
+        return this.type;
+    }
 
     /**
      * Getter $name
      * @return {string }
      */
-	public get $name(): string  {
-		return this.name;
-	}
+    public get $name(): string {
+        return this.name;
+    }
 
     /**
      * Getter $ref
      * @return {HTMLElement }
      */
-	public get $ref(): HTMLElement  {
-		return this.ref;
-	}
+    public get $ref(): HTMLElement {
+        return this.ref;
+    }
 
     /**
      * Getter $id
      * @return {number }
      */
-	public get $id(): number  {
-		return this.id;
-	}
+    public get $id(): number {
+        return this.id;
+    }
 
     /**
      * Setter $moveStatus
      * @param {string } value
      */
-	public set $moveStatus(value: string ) {
-		this.moveStatus = value;
-	}
+    public set $moveStatus(value: string) {
+        this.moveStatus = value;
+    }
 
     /**
      * Setter $type
      * @param {string } value
      */
-	public set $type(value: string ) {
-		this.type = value;
-	}
+    public set $type(value: string) {
+        this.type = value;
+    }
 
     /**
      * Setter $name
      * @param {string } value
      */
-	public set $name(value: string ) {
-		this.name = value;
-	}
+    public set $name(value: string) {
+        this.name = value;
+    }
 
     /**
      * Setter $ref
      * @param {HTMLElement } value
      */
-	public set $ref(value: HTMLElement ) {
-		this.ref = value;
-	}
+    public set $ref(value: HTMLElement) {
+        this.ref = value;
+    }
 
     /**
      * Setter $id
      * @param {string } value
      */
-	public set $id(value: number ) {
-		this.id = value;
+    public set $id(value: number) {
+        this.id = value;
     }
 
 
@@ -184,38 +230,61 @@ class Substance {
      * Getter $speed
      * @return {number }
      */
-	public get $speed(): number  {
-		return this.speed;
-	}
+    public get $speed(): number {
+        return this.speed;
+    }
 
     /**
      * Setter $speed
      * @param {number } value
      */
-	public set $speed(value: number ) {
-		this.speed = value;
-	}
-
+    public set $speed(value: number) {
+        this.speed = value;
+    }
+    /**
+     * Getter $x
+     * @param {number } value
+     */
+    public get $x() {
+        return this.x;
+    }
     /**
      * Setter $x
      * @param {number } value
      */
-	public set $x(value: number ) {
-        if(value>=0 && value<=window.outerWidth){
-            this.x = value;
-        }
-	}
-
+    public set $x(value: number) {
+        this.x = value;
+    }
+    /**
+     * Getter $y
+     * @param {number } value
+     */
+    public get $y() {
+        return this.y;
+    }
     /**
      * Setter $y
      * @param {number } value
      */
-	public set $y(value: number ) {
-		if(value>=0 && value<=window.outerHeight){
-            this.y = value;
-        }
-	}
-    
-    
+    public set $y(value: number) {
+        this.y = value;
+    }
+
+    /**
+     * Getter $panel
+     * @param {number } value
+     */
+    public get $panel() {
+        return this.panel;
+    }
+    /**
+     * Setter $panel
+     * @param {HTMLElement} value
+     */
+    public set $panel(value: HTMLElement) {
+        this.panel = value;
+    }
+
+
 }
 export default Substance;
